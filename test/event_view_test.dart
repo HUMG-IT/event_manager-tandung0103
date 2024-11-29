051d84a1ee87bd1146442d1dc416194f4297f52b
tandung0103 <ntandung112003@gmail.com>
2024-11-29 10:35:24 +0700

    cập nhật

--- test/event_view_test.dart
+++ test/event_view_test.dart
@@ -3,9 +3,7 @@ import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:syncfusion_flutter_calendar/calendar.dart';
-
 import 'package:event_manager/event/event_view.dart';
-
 void main() {
   testWidgets(
       'EventView hiển thị danh sách sự kiện và tương tác với người dùng',
@@ -24,16 +22,12 @@ void main() {
         home: EventView(),
       ),
     );
-
     // Lấy AppLocalizations
     final al = await AppLocalizations.delegate.load(const Locale('vi'));
-
     // Kiểm tra tiêu đề ứng dụng
     expect(find.text(al.appTitle), findsOneWidget);
-
     // Kiểm tra nút chuyển đổi chế độ xem lịch
     expect(find.byType(PopupMenuButton<CalendarView>), findsOneWidget);
-
     // Kiểm tra sự kiện giả (nếu có)
     // Lưu ý: Bạn cần mock dữ liệu hoặc thiết lập trạng thái cho `items` trong `EventView`
     // Nếu không, danh sách sự kiện sẽ rỗng và không hiển thị gì
--- test/main_test.dart
+++ test/main_test.dart
@@ -1,33 +1,17 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:event_manager/main.dart';
 import 'package:event_manager/event/event_view.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
-
 void main() {
-  testWidgets('MyApp khởi chạy và hiển thị EventView',
+  testWidgets('MainApp khởi chạy và hiển thị EventView',
       (WidgetTester tester) async {
-    await tester.pumpWidget(const MaterialApp(
-      localizationsDelegates: [
-        AppLocalizations.delegate,
-        GlobalMaterialLocalizations.delegate,
-        GlobalWidgetsLocalizations.delegate,
-        GlobalCupertinoLocalizations.delegate,
-      ],
-      supportedLocales: [
-        Locale('en'),
-        Locale('vi'),
-      ],
-      locale: Locale("vi"),
-      home: EventView(),
-    ));
-
-    // Tải AppLocalizations cho locale 'vi'
+    // Xây dựng ứng dụng
+    await tester.pumpWidget(const MainApp());
+    // Lấy AppLocalizations
     final al = await AppLocalizations.delegate.load(const Locale('vi'));
-
     // Kiểm tra xem tiêu đề ứng dụng được hiển thị
     expect(find.text(al.appTitle), findsOneWidget);
-
     // Kiểm tra xem EventView được hiển thị
     expect(find.byType(EventView), findsOneWidget);
   });
